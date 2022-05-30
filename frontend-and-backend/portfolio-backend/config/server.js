67d0092f0bcf2d59440ca923956b278d66a0ab0c
Christopher Simanjuntak <manuel.christopher@bitbuy.ca>
2022-05-30 13:03:21 -0400

    first commit

---
+++ frontend-and-backend/portfolio-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4e19fa0a1053997ba20ecffaf72b70ab'),
+    },
+  },
+});
---
+++ frontend-and-backend/portfolio-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3bae8d40-47fe-413e-ba53-4e2fca943901'
+};
